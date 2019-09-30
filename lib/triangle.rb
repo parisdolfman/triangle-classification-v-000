a59e638ff73193af8577a66352ce5231e10525d7
Paris Dolfman <parisdolfman@gmail.com>
2019-09-30 16:28:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,14 +8,8 @@ class Triangle
   @c = c
  end
 
- def valid
-   triangle = [(a + b > c), (a + c > b), (b + c > a)]
-   [a, b, c].each {|x| triangle << false if x <= 0}
-   raise TriangleError if triangle.include?(false)
- end
 
   def kind
-    valid
     if a == b && b == c
       :equilateral
     elsif a == b || a == c || b == c
@@ -25,7 +19,5 @@ class Triangle
       end
     end
 
-  class TriangleError < StandardError
-  end
 
 end
