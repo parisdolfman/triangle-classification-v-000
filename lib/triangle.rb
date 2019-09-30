f51c876e3da35bd742e868588c8cc19418a7760c
Paris Dolfman <parisdolfman@gmail.com>
2019-09-30 17:05:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,17 +8,6 @@ class Triangle
   @c = c
  end
 
- def validate_triangle
-   valid_triangle = [
-     (a_side + b_side > c_side),
-     (a_side + c_side > b_side),
-     (b_side + c_side > a_side)
-   ]
-   [a_side, b_side, c_side].each do |side|
-     valid_triangle << false if side <= 0
-     raise TriangleError if valid_triangle.include?(false)
-   end
- end
 
   def kind
     validate_triangle
@@ -30,6 +19,18 @@ class Triangle
         :scalene
       end
     end
+
+    def validate_triangle
+      valid_triangle = [
+        (a_side + b_side > c_side),
+        (a_side + c_side > b_side),
+        (b_side + c_side > a_side)
+      ]
+      [a_side, b_side, c_side].each do |side|
+        valid_triangle << false if side <= 0
+        raise TriangleError if valid_triangle.include?(false)
+      end
+    end  
 
 
 
